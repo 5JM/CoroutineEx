35bba7d752b033390eba67ebb34c2da342e63bd6
5JM <toddlf0614@naver.com>
2022-03-31 21:55:22 +0900

    update:readme

--- app/src/main/java/com/jm/coroutineex/MainActivity.kt
+++ app/src/main/java/com/jm/coroutineex/MainActivity.kt
@@ -19,26 +19,70 @@ class MainActivity : AppCompatActivity() {
 
         val formatter = DateTimeFormatter.ISO_LOCAL_TIME
 
-        val job = CoroutineScope(Dispatchers.Default).async{
-            val as1 = async{
-                Log.e("Jaemu>>", "as1 start : "+formatter.format(LocalDateTime.now()))
-                delay(500)
-                Log.e("Jaemu>>", "as1 end : "+formatter.format(LocalDateTime.now()))
-                200
-            }
+        // launch , join
+//        val job_l = CoroutineScope(Dispatchers.Default).launch{
+//            launch{
+//                for (i in 0..5){
+//                    delay(500)
+//                    Log.e("Jaemu>>","코루틴 $i")
+//                }
+//            }.join() // 1. join 선언시 해당 launch블록이 먼저 실행
+//
+//            launch{
+//                for (i in 6..10){
+//                    delay(500)
+//                    Log.e("Jaemu>>","코루틴 $i")
+//                }
+//            }
+//        }
 
-            val as2 = async{
-                Log.e("Jaemu>>", "as2 start : "+formatter.format(LocalDateTime.now()))
-                delay(1000)
-                Log.e("Jaemu>>", "as2 end : "+formatter.format(LocalDateTime.now()))
-                300
-            }
+        //  async , await
+//        val job_a = CoroutineScope(Dispatchers.Default).async{
+//            val as1 = async{
+//                Log.e("Jaemu>>", "as1 start : "+formatter.format(LocalDateTime.now()))
+//                delay(500)
+//                Log.e("Jaemu>>", "as1 end : "+formatter.format(LocalDateTime.now()))
+//                200
+//            }
+//
+//            val as2 = async{
+//                Log.e("Jaemu>>", "as2 start : "+formatter.format(LocalDateTime.now()))
+//                delay(1000)
+//                Log.e("Jaemu>>", "as2 end : "+formatter.format(LocalDateTime.now()))
+//                300
+//            }
+//
+//            Log.e("Jaemu>>","${as1.await()} + ${as2.await()}")
+//        }
 
-            Log.e("Jaemu>>","${as1.await()} + ${as2.await()}")
+        // suspend
+        suspend fun suspendFunc(){
+            for(i in 0..10){
+                Log.e("Jaemu>>","$i")
+            }
         }
 
-        button.setOnClickListener {
-            job.cancel()
+        CoroutineScope(Dispatchers.Main).launch {
+            launch{
+                for (i in 11..20){
+                    delay(500)
+                    Log.e("Jaemu>>","코루틴 $i")
+                }
+            }
+
+            suspendFunc()
+
+            launch{
+                for (i in 21..30){
+                    delay(500)
+                    Log.e("Jaemu>>","코루틴 $i")
+                }
+            }
         }
+
+
+//        button.setOnClickListener {
+//            job.cancel()
+//        }
     }
 }
